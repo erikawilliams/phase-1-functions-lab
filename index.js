78e6574778d079b1cb8b331656bcb27ddfdcca21
Erika Williams <erika@erikawilliams.us>
2022-03-14 16:36:38 -0600

    commit tree bd66c32996d620d25c1848063bea49dd4adc91a7

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(value) {
+    let distance = Math.abs(value - 42)
+    return distance
+}
+
+function distanceFromHqInFeet(value) {
+    let distance = distanceFromHqInBlocks(value) * 264
+    return distance
+}
+
+function distanceTravelledInFeet(start, end) {
+    let distance = Math.abs(end-start)*264
+    return distance
+}
+
+function calculatesFarePrice(start, end) {
+    let distance = distanceTravelledInFeet(start,end)
+    let cost
+    if (distance <= 400) {
+        cost = 0
+        return cost
+    } else if (distance < 2000) {
+        cost = (distance-400)* 0.02
+        return cost
+    } else if (distance <2500) {
+        cost = 25
+        return cost
+    } else {
+        return "cannot travel that far"
+    }
+}
